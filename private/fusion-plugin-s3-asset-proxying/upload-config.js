bd038ff08d20923e5f2d52daa6297722303ee42c
Dave Kearns <dkearns@uber.com>
2017-10-19 13:52:17 -0700

    Upload CLI
    Reviewers: ganemone, dennis.lin, rtsao, amsmith, lhorie
    
    Reviewed By: dennis.lin
    
    Differential Revision: https://code.uberinternal.com/D1262193

---
+++ private/fusion-plugin-s3-asset-proxying/upload-config.js
@@ -0,0 +1,23 @@
+/*eslint-env node */
+const util = require('util');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async function getConfig({
+  secretsPath = path.join(process.cwd(), 'config/secrets/secrets.json'),
+}) {
+  const {
+    s3: {
+      aws: prefix,
+      aws_access_key_id: accessKeyId,
+      aws_secret_access_key: secretAccessKey,
+    },
+  } = JSON.parse(await util.promisify(fs.readFile)(secretsPath));
+
+  return {
+    bucket: 'uber-common-private',
+    prefix,
+    accessKeyId,
+    secretAccessKey,
+  };
+};
---
+++ private/fusion-plugin-s3-asset-proxying/upload.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+/* eslint-env node */
+const util = require('util');
+const fs = require('fs');
+const zlib = require('zlib');
+const path = require('path');
+const mime = require('mime');
+const mimeDb = require('mime-db');
+const AWS = require('aws-sdk');
+
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+(async () => {
+  const directory = path.join(
+    process.cwd(),
+    '.framework/dist/production/client'
+  );
+
+  const files = await util.promisify(fs.readdir)(directory);
+
+  const {
+    bucket,
+    prefix,
+    accessKeyId,
+    secretAccessKey,
+  } = await require('./upload-config.js')();
+  const s3 = new AWS.S3({
+    accessKeyId,
+    secretAccessKey,
+  });
+
+  return Promise.all(
+    files.map(async filename => {
+      const uploadPath = path.join(prefix, filename);
+      if (uploadPath.endsWith('.map')) {
+        // eslint-disable-next-line no-console
+        console.log(`Skipping upload of sourcemap: ${uploadPath}`);
+        return Promise.resolve();
+      }
+      const mimeType = mime.lookup(uploadPath);
+      const mimeInfo = mimeDb[mimeType];
+
+      const fileBuffer = await util.promisify(fs.readFile)(
+        path.join(directory, filename)
+      );
+
+      const uploadBuffer = mimeInfo.compressible
+        ? await util.promisify(zlib.gzip)(fileBuffer)
+        : fileBuffer;
+
+      return new Promise((resolve, reject) => {
+        const contentLength = uploadBuffer.length;
+        // eslint-disable-next-line no-console
+        console.log(`Uploading: ${uploadPath} - ${contentLength} bytes`);
+        return s3.putObject(
+          {
+            Bucket: bucket,
+            Key: uploadPath,
+            Body: uploadBuffer,
+            ContentType: mimeType,
+            ContentLength: contentLength,
+            CacheControl: 'max-age=31536000',
+            ContentEncoding: mimeInfo.compressible ? 'gzip' : null,
+          },
+          function(uploadError) {
+            if (
+              uploadError ||
+              !this.httpResponse ||
+              this.httpResponse.statusCode !== 200
+            ) {
+              // eslint-disable-next-line no-console
+              console.log(`Error uploading: ${uploadPath}`);
+              return reject(
+                uploadError ||
+                  new Error(
+                    `Failed to upload ${uploadPath}: ${this.httpResponse}`
+                  )
+              );
+            }
+
+            // eslint-disable-next-line no-console
+            console.log(
+              `Sucessfully uploaded: ${uploadPath} - ${contentLength} bytes`
+            );
+            return resolve();
+          }
+        );
+      });
+    })
+  );
+})();
