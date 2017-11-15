53c640946efe25d4327cf49f0ad6513aabb9f1e8
Giancarlo Anemone <ganemone@uber.com>
2017-11-14 18:52:36 -0800

    Update dependencies
    Differential Revision: https://code.uberinternal.com/D1325343

--- private/fusion-plugin-logtron/.cuprc.js
+++ private/fusion-plugin-logtron/.cuprc.js
@@ -1,5 +1,5 @@
 module.exports = {
   babel: {
-     plugins: process.env.NODE_ENV === 'test' ? [require.resolve('babel-plugin-istanbul')] : [],
+     plugins: process.env.NODE_ENV === 'test' ? [require.resolve('@babel/plugin-istanbul')] : [],
   },
 };
