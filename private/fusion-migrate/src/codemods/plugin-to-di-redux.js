f26252b231747f3e8d5317be3ff0228bc7f97b7f
Leo Horie <lhorie@uber.com>
2018-01-08 13:09:02 -0800

    update token origin
    Differential Revision: https://code.uberinternal.com/D1426699

--- private/fusion-migrate/src/codemods/plugin-to-di-redux.js
+++ private/fusion-migrate/src/codemods/plugin-to-di-redux.js
@@ -4,8 +4,7 @@ module.exports = compose(
   ({source}) => {
     return source.replace(
       `import ReactReduxPlugin from 'fusion-plugin-react-redux';`,
-      `import {ReduxToken} from 'fusion-tokens';
-import ReactReduxPlugin, {ReduxConfigToken} from 'fusion-plugin-react-redux';`
+      `import ReactReduxPlugin, {ReduxToken, ReduxConfigToken} from 'fusion-plugin-react-redux';`
     );
   },
   ({source}) => {
