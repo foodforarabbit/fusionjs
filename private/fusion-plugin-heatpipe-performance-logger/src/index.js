6c8e945a5827f2bb8622d784662f212d99133009
Angus Croll <angus@uber.com>
2018-01-18 15:58:14 -0800

    initial

--- private/fusion-plugin-heatpipe-performance-logger/src/index.js
+++ private/fusion-plugin-heatpipe-performance-logger/src/index.js
@@ -1,3 +1,26 @@
-module.exports = fusionPluginHeatpipePerformanceLogger;
+/* global console */
+/* eslint-disable no-console */
 
-function fusionPluginHeatpipePerformanceLogger() {}
+import {createHeatpipePublisher, publishToHeatpipe} from '@uber/heatpipe-web-performance-logger';
+
+module.exports = ({EventEmitter, config}) => {
+  if (__NODE__) {
+    if (!EventEmitter) {
+      throw new Error(`EventEmitter is required, but was: ${EventEmitter}`);
+    }
+
+    const publisher = createHeatpipePublisher({
+      error: console.log,
+      info: console.log,
+      debug: console.log
+    },
+      {
+        increment: () => {}
+      }, config);
+
+    const emitter = EventEmitter.of();
+    emitter.on('browser-performance-emitter:stats', (e, ctx) => {
+      publishToHeatpipe(publisher, e);
+    });
+  }
+};
