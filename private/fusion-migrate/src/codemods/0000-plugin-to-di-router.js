54038e9af6e01f0e16c540be0faff7d16108ceff
Leo Horie <lhorie@uber.com>
2018-01-25 14:30:47 -0800

    migrate test-utils

--- private/fusion-migrate/src/codemods/0000-plugin-to-di-router.js
+++ private/fusion-migrate/src/codemods/0000-plugin-to-di-router.js
@@ -4,6 +4,5 @@ const migrate = require('../utils/plugin-to-di-standalone');
 
 module.exports = compose(
   bump('fusion-plugin-react-router', '0.4.0'),
-  migrate('Router'),
-  migrate('RouterPlugin')
+  migrate('Router')
 );
---
+++ private/fusion-migrate/src/codemods/0000-test-utils-test-app.js
@@ -0,0 +1,45 @@
+module.exports = ({source}) =>
+  source.replace(
+    `import App from 'fusion-react';
+import Styletron from 'fusion-plugin-styletron-react';
+import Enzyme, {mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import RouterPlugin from 'fusion-plugin-react-router';
+
+import DefaultRoot from '../components/root';
+
+Enzyme.configure({adapter: new Adapter()});
+
+export default function start(options = {}) {
+  const defaultRender = mount;
+  const render = options.render || defaultRender;
+  const app = new App(options.root || DefaultRoot, render);
+  app.plugin(Styletron);
+  app.plugin(RouterPlugin);
+
+  return app;
+}`,
+    `import App from 'fusion-react';
+import Styletron from 'fusion-plugin-styletron-react';
+import Enzyme, {mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Router from 'fusion-plugin-react-router';
+import UniversalEvents, {
+  UniversalEventsToken,
+} from 'fusion-plugin-universal-events';
+
+import DefaultRoot from '../components/root';
+
+Enzyme.configure({adapter: new Adapter()});
+
+export default function start(options = {}) {
+  const defaultRender = mount;
+  const render = options.render || defaultRender;
+  const app = new App(options.root || DefaultRoot, render);
+  app.register(Styletron);
+  app.register(Router);
+  app.register(UniversalEventsToken, UniversalEvents);
+
+  return app;
+}`
+  );
