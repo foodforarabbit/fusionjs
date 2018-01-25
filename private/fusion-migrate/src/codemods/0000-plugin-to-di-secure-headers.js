ed61e19bfb31587ebec2db87221102866043ffa8
Leo Horie <lhorie@uber.com>
2018-01-25 12:26:11 -0800

    clean up secure headers

--- private/fusion-migrate/src/codemods/0000-plugin-to-di-secure-headers.js
+++ private/fusion-migrate/src/codemods/0000-plugin-to-di-secure-headers.js
@@ -9,12 +9,19 @@ module.exports = compose(
       `import SecureHeaders, {
   SecureHeadersToken,
   SecureHeadersServiceNameConfigToken,
+  SecureHeadersCSPConfigToken,
 } from '@uber/fusion-plugin-secure-headers';`
+    ),
+  ({source}) =>
+    source.replace(
+      `const {team, service} = metaConfig;`,
+      `const {team} = metaConfig;`
     ),
   ({source}) =>
     source.replace(
       `app.plugin(SecureHeaders, {config: secureHeadersConfig});`,
       `app.register(SecureHeadersToken, SecureHeaders);
-  app.register(SecureHeadersServiceNameConfigToken, service);`
+  app.register(SecureHeadersServiceNameConfigToken, secureHeadersConfig.service);
+  app.register(SecureHeadersCSPConfigToken, secureHeadersConfig.csp);`
     )
 );
--- private/fusion-migrate/src/codemods/0001-merge-fusion-tokens-imports.js
+++ private/fusion-migrate/src/codemods/0001-merge-fusion-tokens-imports.js
@@ -4,14 +4,13 @@ module.exports = ({source}) => {
     source.match(`import {FetchToken} from 'fusion-tokens';`) &&
     source.match(`import {LoggerToken} from 'fusion-tokens';`)
   ) {
-    return source.replace(
-      `import {SessionToken} from 'fusion-tokens';`,
-      `import {
-  FetchToken,
-  LoggerToken,
-  SessionToken,
-} from 'fusion-tokens';`
-    );
+    return source
+      .replace(
+        `import {SessionToken} from 'fusion-tokens';`,
+        `import {FetchToken, LoggerToken, SessionToken} from 'fusion-tokens';`
+      )
+      .replace(`import {FetchToken} from 'fusion-tokens';`, '')
+      .replace(`import {LoggerToken} from 'fusion-tokens';`, '');
   }
   return source;
 };
