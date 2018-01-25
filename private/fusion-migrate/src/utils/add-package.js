8bff67d7e0b8c0370d1d9bb3766ee2cfe356e85c
Leo Horie <lhorie@uber.com>
2018-01-25 13:49:42 -0800

    improve add heuristic: if it's a fusion package, add dependency as peer/dev dev, otherwise as regular dep

--- private/fusion-migrate/src/utils/add-package.js
+++ private/fusion-migrate/src/utils/add-package.js
@@ -9,7 +9,8 @@ module.exports = (depType, name, version) => ({source}) => {
     const files = find.fileSync(/package\.json/, process.cwd());
     files.forEach(file => {
       const meta = JSON.parse(fs.readFileSync(file, 'utf-8'));
-      if (meta[depType] && meta[depType][name]) meta[depType][name] = version;
+      if (!meta[depType]) meta[depType] = {};
+      meta[depType][name] = version;
       fs.writeFileSync(file, JSON.stringify(meta, null, 2), 'utf8');
     });
   }
