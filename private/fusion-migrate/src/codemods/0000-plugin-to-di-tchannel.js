0ce51f29fd5a7f5b208e27465d57943408873302
Leo Horie <lhorie@uber.com>
2018-01-25 14:51:14 -0800

    fix import in tchannel codemod

--- private/fusion-migrate/src/codemods/0000-plugin-to-di-tchannel.js
+++ private/fusion-migrate/src/codemods/0000-plugin-to-di-tchannel.js
@@ -6,14 +6,13 @@ module.exports = compose(
   ({source}) => {
     return source.replace(
       `import TChannelPlugin from '@uber/fusion-plugin-tchannel';`,
-      `import TChannelToken from '@uber/fusion-tokens';
-import TChannelPlugin from '@uber/fusion-plugin-tchannel';`
+      `import TChannel, {TChannelToken} from '@uber/fusion-plugin-tchannel';`
     );
   },
   ({source}) => {
     return source.replace(
       `const TChannel = app.plugin(TChannelPlugin, {service, Logger, M3});`,
-      `app.register(TChannelToken, TChannelPlugin);`
+      `app.register(TChannelToken, TChannel);`
     );
   }
 );
