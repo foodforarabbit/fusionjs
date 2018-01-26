163ec7a3413d1439b9b86d202a8277e08fe1b978
Leo Horie <lhorie@uber.com>
2018-01-26 11:18:48 -0800

    bump jwt

--- private/fusion-migrate/src/codemods/0000-plugin-to-di-jwt.js
+++ private/fusion-migrate/src/codemods/0000-plugin-to-di-jwt.js
@@ -2,7 +2,7 @@ const compose = require('../utils/compose');
 const bump = require('../utils/bump-version');
 
 module.exports = compose(
-  bump('fusion-plugin-jwt', '0.3.0'),
+  bump('fusion-plugin-jwt', '0.3.1'),
   ({source}) => {
     return source.replace(
       `import JWTSessionPlugin from 'fusion-plugin-jwt';`,
