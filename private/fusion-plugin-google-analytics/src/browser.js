e4f25cf8c1e1529f578cf3713c852416680facd6
Giancarlo Anemone <ganemone@uber.com>
2018-01-26 17:12:18 -0800

    Fix linting
    Reviewers: #web_platform, lhorie
    
    Reviewed By: #web_platform, lhorie
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D1464317

--- private/fusion-plugin-google-analytics/src/browser.js
+++ private/fusion-plugin-google-analytics/src/browser.js
@@ -5,96 +5,97 @@ import loadGoogleAnalytics from './load-ga';
 
 export const GoogleAnalyticsConfigToken = createToken('GoogleAnalyticsConfig');
 
-export default createPlugin({
-  deps: {
-    options: GoogleAnalyticsConfigToken,
-  },
-  provides: ({options}) => {
-    if (!options.trackingId) {
-      throw new Error('Tracking id required');
-    }
-    options = Object.assign(
-      {
-        advertiserFeatures: false,
-        anonymizeIp: false,
-        cookieDomain: 'auto',
-        linkAttribution: false,
-        trackPage: false,
-        loadGA: loadGoogleAnalytics,
-      },
-      options
-    );
-    if (!options.name) {
-      // TODO: Maybe get the name from config.meta.project
-      // Note: Google Analytics does not like '-' in the name
-      // Tracker name is required in all subsequent command queue calls, see docs:
-      // https://developers.google.com/analytics/devguides/collection/analyticsjs/command-queue-reference
-      options.name = options.trackingId.replace(/-/g, '_');
-    }
-    class GoogleAnalytics {
-      constructor() {
-        this.options = options;
-
-        // load ga script
-        options.loadGA();
-
-        this._ga = options.mock || window.ga;
-
-        // Create named tracker
-        this._ga(
-          'create',
-          options.trackingId,
-          options.cookieDomain,
-          options.name
-        );
-        // Initialize ga tracker features
-        this._initializeFeatures();
+export default __BROWSER__ &&
+  createPlugin({
+    deps: {
+      options: GoogleAnalyticsConfigToken,
+    },
+    provides: ({options}) => {
+      if (!options.trackingId) {
+        throw new Error('Tracking id required');
       }
-
-      ga(name, ...args) {
-        this._ga(this.options.name + name, ...args);
+      options = Object.assign(
+        {
+          advertiserFeatures: false,
+          anonymizeIp: false,
+          cookieDomain: 'auto',
+          linkAttribution: false,
+          trackPage: false,
+          loadGA: loadGoogleAnalytics,
+        },
+        options
+      );
+      if (!options.name) {
+        // TODO: Maybe get the name from config.meta.project
+        // Note: Google Analytics does not like '-' in the name
+        // Tracker name is required in all subsequent command queue calls, see docs:
+        // https://developers.google.com/analytics/devguides/collection/analyticsjs/command-queue-reference
+        options.name = options.trackingId.replace(/-/g, '_');
       }
+      class GoogleAnalytics {
+        constructor() {
+          this.options = options;
 
-      // https://developers.google.com/analytics/devguides/collection/analyticsjs/cookies-user-id
-      identify(id) {
-        this.ga('.set', 'userId', id);
-      }
+          // load ga script
+          options.loadGA();
 
-      // https://developers.google.com/analytics/devguides/collection/analyticsjs/events
-      track(data) {
-        this.ga('.send', 'event', data);
-      }
+          this._ga = options.mock || window.ga;
 
-      // https://developers.google.com/analytics/devguides/collection/analyticsjs/pages
-      pageview({title, page, location}) {
-        this.ga('.set', {
-          title: title || document.title,
-          page: page || window.location.pathname,
-          location: location || window.location.href,
-        });
-        this.ga('.send', 'pageview');
-      }
-
-      _initializeFeatures() {
-        var options = this.options;
-
-        if (options.advertiserFeatures) {
-          this.ga('.require', 'displayfeatures');
+          // Create named tracker
+          this._ga(
+            'create',
+            options.trackingId,
+            options.cookieDomain,
+            options.name
+          );
+          // Initialize ga tracker features
+          this._initializeFeatures();
         }
 
-        if (options.linkAttribution) {
-          this.ga('.require', 'linkid', 'linkid.js');
+        ga(name, ...args) {
+          this._ga(this.options.name + name, ...args);
         }
 
-        if (options.trackPage) {
+        // https://developers.google.com/analytics/devguides/collection/analyticsjs/cookies-user-id
+        identify(id) {
+          this.ga('.set', 'userId', id);
+        }
+
+        // https://developers.google.com/analytics/devguides/collection/analyticsjs/events
+        track(data) {
+          this.ga('.send', 'event', data);
+        }
+
+        // https://developers.google.com/analytics/devguides/collection/analyticsjs/pages
+        pageview({title, page, location}) {
+          this.ga('.set', {
+            title: title || document.title,
+            page: page || window.location.pathname,
+            location: location || window.location.href,
+          });
           this.ga('.send', 'pageview');
         }
 
-        if (options.anonymizeIp) {
-          this.ga('.set', 'anonymizeIp', true);
+        _initializeFeatures() {
+          var options = this.options;
+
+          if (options.advertiserFeatures) {
+            this.ga('.require', 'displayfeatures');
+          }
+
+          if (options.linkAttribution) {
+            this.ga('.require', 'linkid', 'linkid.js');
+          }
+
+          if (options.trackPage) {
+            this.ga('.send', 'pageview');
+          }
+
+          if (options.anonymizeIp) {
+            this.ga('.set', 'anonymizeIp', true);
+          }
         }
       }
-    }
-    return new GoogleAnalytics();
-  },
-});
+      return new GoogleAnalytics();
+    },
+  });
--- private/fusion-plugin-google-analytics/src/server.js
+++ private/fusion-plugin-google-analytics/src/server.js
@@ -1,14 +1,15 @@
 /* eslint-env node */
 import {createPlugin, html} from 'fusion-core';
 
-export default createPlugin({
-  middleware: () => {
-    const escaped = html`<script async src='https://www.google-analytics.com/analytics.js'></script>`;
-    return function middleware(ctx, next) {
-      if (ctx.element) {
-        ctx.body.head.push(escaped);
-      }
-      return next();
-    };
-  },
-});
+export default __NODE__ &&
+  createPlugin({
+    middleware: () => {
+      const escaped = html`<script async src='https://www.google-analytics.com/analytics.js'></script>`;
+      return function middleware(ctx, next) {
+        if (ctx.element) {
+          ctx.body.head.push(escaped);
+        }
+        return next();
+      };
+    },
+  });
