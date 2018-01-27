70973125eca3e9d2258d8194cde4ed54a677fb3e
Giancarlo Anemone <ganemone@uber.com>
2018-01-26 17:12:19 -0800

    Fix linting
    Reviewers: #web_platform, lhorie
    
    Reviewed By: #web_platform, lhorie
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D1464331

--- private/fusion-plugin-secure-headers/src/browser.js
+++ private/fusion-plugin-secure-headers/src/browser.js
@@ -1,10 +1,11 @@
 import {createPlugin} from 'fusion-core';
 
 /* eslint-env browser */
-export default createPlugin({
-  provides: () => ({
-    from() {
-      throw new Error('Cannot call SecureHeaders.from() in the browser');
-    },
-  }),
-});
+export default __BROWSER__ &&
+  createPlugin({
+    provides: () => ({
+      from() {
+        throw new Error('Cannot call SecureHeaders.from() in the browser');
+      },
+    }),
+  });
--- private/fusion-plugin-secure-headers/src/server.js
+++ private/fusion-plugin-secure-headers/src/server.js
@@ -12,37 +12,38 @@ import buildCSPMiddleware from './csp/middleware.js';
 
 import {REQUIRED_CSP_CONTENT_TYPES, CSP_HEADERS} from './constants.js';
 
-export default createPlugin({
-  deps: {
-    serviceName: SecureHeadersServiceNameConfigToken,
-    useFrameGuard: SecureHeadersUseFrameguardConfigToken,
-    cspConfig: SecureHeadersCSPConfigToken,
-  },
-  middleware: ({serviceName, useFrameguard, cspConfig}) => async (
-    ctx,
-    next
-  ) => {
-    const secureHeaderMiddlewares = [];
-    secureHeaderMiddlewares.push(
-      buildCSPMiddleware({
-        ctx,
-        serviceName,
-        cspConfig,
-      })
-    );
-    if (useFrameguard !== false) {
+export default __NODE__ &&
+  createPlugin({
+    deps: {
+      serviceName: SecureHeadersServiceNameConfigToken,
+      useFrameGuard: SecureHeadersUseFrameguardConfigToken,
+      cspConfig: SecureHeadersCSPConfigToken,
+    },
+    middleware: ({serviceName, useFrameguard, cspConfig}) => async (
+      ctx,
+      next
+    ) => {
+      const secureHeaderMiddlewares = [];
       secureHeaderMiddlewares.push(
-        koaHelmet.frameguard({action: 'sameorigin'})
+        buildCSPMiddleware({
+          ctx,
+          serviceName,
+          cspConfig,
+        })
       );
-    }
-    secureHeaderMiddlewares.push(koaHelmet.xssFilter());
+      if (useFrameguard !== false) {
+        secureHeaderMiddlewares.push(
+          koaHelmet.frameguard({action: 'sameorigin'})
+        );
+      }
+      secureHeaderMiddlewares.push(koaHelmet.xssFilter());
 
-    await compose(secureHeaderMiddlewares)(ctx, next);
+      await compose(secureHeaderMiddlewares)(ctx, next);
 
-    // TODO: move everything to post-render since it depends on content-type
-    // If the content type does not require CSP headers then remove them
-    if (!REQUIRED_CSP_CONTENT_TYPES.includes(ctx.type)) {
-      CSP_HEADERS.forEach(h => ctx.remove(h));
-    }
-  },
-});
+      // TODO: move everything to post-render since it depends on content-type
+      // If the content type does not require CSP headers then remove them
+      if (!REQUIRED_CSP_CONTENT_TYPES.includes(ctx.type)) {
+        CSP_HEADERS.forEach(h => ctx.remove(h));
+      }
+    },
+  });
