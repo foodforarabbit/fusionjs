e20aebf0dea26215e1510618d445a7bb40f2758d
Giancarlo Anemone <ganemone@uber.com>
2018-01-26 17:12:18 -0800

    Fix linting
    Reviewers: #web_platform, lhorie
    
    Reviewed By: #web_platform, lhorie
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D1464327

--- private/fusion-plugin-rosetta/src/server.js
+++ private/fusion-plugin-rosetta/src/server.js
@@ -7,27 +7,28 @@ import Genghis from '@uber/node-genghis';
 export const ClientToken = createOptionalToken('RosettaClient', Genghis);
 export const ConfigToken = createOptionalToken('RosettaConfig', {});
 
-export default createPlugin({
-  deps: {
-    logger: LoggerToken,
-    Client: ClientToken,
-    config: ConfigToken,
-  },
-  provides: deps => {
-    const {logger, Client, config} = deps;
-    config.service = config.service || process.env.SVC_ID || 'dev-service';
-    const client = new Client({logger, ...config});
-    client.load();
-    client.setLoadInterval();
-    const supportedLocales = new Locales(client.locales);
-    class TranslationsLoader {
-      constructor(ctx) {
-        const expectedLocales = new Locales(ctx.headers['accept-language']);
-        this.locale = expectedLocales.best(supportedLocales); // TODO does it need to read from cookie __LOCALE__ or from requestedLocale?
-        this.translations = client.translations[this.locale.normalized];
+export default __NODE__ &&
+  createPlugin({
+    deps: {
+      logger: LoggerToken,
+      Client: ClientToken,
+      config: ConfigToken,
+    },
+    provides: deps => {
+      const {logger, Client, config} = deps;
+      config.service = config.service || process.env.SVC_ID || 'dev-service';
+      const client = new Client({logger, ...config});
+      client.load();
+      client.setLoadInterval();
+      const supportedLocales = new Locales(client.locales);
+      class TranslationsLoader {
+        constructor(ctx) {
+          const expectedLocales = new Locales(ctx.headers['accept-language']);
+          this.locale = expectedLocales.best(supportedLocales); // TODO does it need to read from cookie __LOCALE__ or from requestedLocale?
+          this.translations = client.translations[this.locale.normalized];
+        }
       }
-    }
-    client.from = memoize(ctx => new TranslationsLoader(ctx));
-    return client;
-  },
-});
+      client.from = memoize(ctx => new TranslationsLoader(ctx));
+      return client;
+    },
+  });
