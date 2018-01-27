e6eec172c21074cfe789129f79bc938055d3b947
Giancarlo Anemone <ganemone@uber.com>
2018-01-26 17:12:18 -0800

    Fix linting
    Reviewers: #web_platform, lhorie
    
    Reviewed By: #web_platform, lhorie
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D1464323

--- private/fusion-plugin-m3/src/browser.js
+++ private/fusion-plugin-m3/src/browser.js
@@ -2,25 +2,26 @@
 import {createPlugin} from 'fusion-core';
 import {UniversalEventsToken} from 'fusion-plugin-universal-events';
 
-export default createPlugin({
-  deps: {events: UniversalEventsToken},
-  provides: ({events}) => {
-    return {
-      counter(key, value, tags) {
-        events.emit('m3:counter', {key, value, tags});
-      },
-      increment(key, tags) {
-        events.emit('m3:increment', {key, tags});
-      },
-      decrement(key, tags) {
-        events.emit('m3:decrement', {key, tags});
-      },
-      timing(key, value, tags) {
-        events.emit('m3:timing', {key, value, tags});
-      },
-      gauge(key, value, tags) {
-        events.emit('m3:gauge', {key, value, tags});
-      },
-    };
-  },
-});
+export default __BROWSER__ &&
+  createPlugin({
+    deps: {events: UniversalEventsToken},
+    provides: ({events}) => {
+      return {
+        counter(key, value, tags) {
+          events.emit('m3:counter', {key, value, tags});
+        },
+        increment(key, tags) {
+          events.emit('m3:increment', {key, tags});
+        },
+        decrement(key, tags) {
+          events.emit('m3:decrement', {key, tags});
+        },
+        timing(key, value, tags) {
+          events.emit('m3:timing', {key, value, tags});
+        },
+        gauge(key, value, tags) {
+          events.emit('m3:gauge', {key, value, tags});
+        },
+      };
+    },
+  });
