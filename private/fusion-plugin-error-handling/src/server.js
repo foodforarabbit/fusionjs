0729811b7b2ed5c71d87f0921a13efec41fb7d90
Giancarlo Anemone <ganemone@uber.com>
2018-01-26 17:12:19 -0800

    Fix linting
    Reviewers: #web_platform, lhorie
    
    Reviewed By: #web_platform, lhorie
    
    Differential Revision: https://code.uberinternal.com/D1464341

--- private/fusion-plugin-error-handling/src/server.js
+++ private/fusion-plugin-error-handling/src/server.js
@@ -3,49 +3,50 @@ import {LoggerToken} from 'fusion-tokens';
 import {M3Token} from '@uber/fusion-plugin-m3';
 
 const defaultTimeout = 10000;
-export default createPlugin({
-  deps: {
-    logger: LoggerToken,
-    m3: M3Token,
-  },
-  provides: ({logger, m3}) => {
-    const errorLog = (e, captureType) => {
-      const defaultMessage = `uncaught ${captureType} exception`;
+export default __NODE__ &&
+  createPlugin({
+    deps: {
+      logger: LoggerToken,
+      m3: M3Token,
+    },
+    provides: ({logger, m3}) => {
+      const errorLog = (e, captureType) => {
+        const defaultMessage = `uncaught ${captureType} exception`;
 
-      let _err = e;
-      if (typeof _err !== 'object') {
-        _err = {
-          message: (typeof e === 'string' && e) || defaultMessage,
+        let _err = e;
+        if (typeof _err !== 'object') {
+          _err = {
+            message: (typeof e === 'string' && e) || defaultMessage,
+          };
+        } else {
+          _err.message = _err.message || defaultMessage;
+        }
+
+        _err.tags = {
+          captureType,
         };
-      } else {
-        _err.message = _err.message || defaultMessage;
-      }
 
-      _err.tags = {
-        captureType,
+        return new Promise(resolve => {
+          logger.fatal(_err.message, _err, resolve);
+        });
+      };
+      const errorIncrement = captureType => {
+        return new Promise(resolve => {
+          m3.immediateIncrement('exception', {captureType}, resolve);
+        });
       };
 
-      return new Promise(resolve => {
-        logger.fatal(_err.message, _err, resolve);
-      });
-    };
-    const errorIncrement = captureType => {
-      return new Promise(resolve => {
-        m3.immediateIncrement('exception', {captureType}, resolve);
-      });
-    };
-
-    return (e, captureType) => {
-      const delayP = delay(defaultTimeout);
-      return Promise.race([
-        Promise.all([errorLog(e, captureType), errorIncrement(captureType)]),
-        delayP,
-      ]).then(() => {
-        delayP.cancel();
-      });
-    };
-  },
-});
+      return (e, captureType) => {
+        const delayP = delay(defaultTimeout);
+        return Promise.race([
+          Promise.all([errorLog(e, captureType), errorIncrement(captureType)]),
+          delayP,
+        ]).then(() => {
+          delayP.cancel();
+        });
+      };
+    },
+  });
 
 function delay(time) {
   let id = null;
