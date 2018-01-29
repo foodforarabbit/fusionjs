44fda96866bd1c220137b93a09a23fb6ec94de53
Leo Horie <lhorie@uber.com>
2018-01-29 15:46:00 -0800

    bump test utils

--- private/fusion-migrate/src/codemods/0000-fusion-core-greenkeeping.js
+++ private/fusion-migrate/src/codemods/0000-fusion-core-greenkeeping.js
@@ -6,6 +6,6 @@ module.exports = compose(
   bump('fusion-core', '0.3.0-5'),
   bump('fusion-cli', '0.3.1'),
   bump('fusion-react', '0.4.1'),
-  bump('fusion-test-utils', '0.4.1'),
+  bump('fusion-test-utils', '0.4.2'),
   add('fusion-tokens', '^0.0.5')
 );
