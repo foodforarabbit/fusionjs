cc4027b00f19f96e3b75e22b1c821121f8808f62
Giancarlo Anemone <ganemone@uber.com>
2018-01-29 14:08:21 -0800

    Add export of GalileoToken
    Reviewers: #web_platform
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D1466715

--- private/fusion-plugin-galileo/src/index.js
+++ private/fusion-plugin-galileo/src/index.js
@@ -1,6 +1,8 @@
 // Main export file
+import {createToken} from 'fusion-tokens';
 import server, {ConfigToken} from './server';
 import browser from './browser';
 
 export default (__NODE__ ? server : browser);
 export const GalileoConfigToken = __NODE__ && ConfigToken;
+export const GalileoToken = createToken('GalileoToken');
