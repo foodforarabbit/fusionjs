052427788de98cd66b724b2acbe1d3e8e809d031
Leo Horie <lhorie@uber.com>
2018-01-31 13:30:28 -0800

    handle njk file

--- private/fusion-migrate/src/utils/add-file.js
+++ private/fusion-migrate/src/utils/add-file.js
@@ -4,6 +4,14 @@ const path = require('path');
 
 const cache = {};
 
+const readPackageJson = f => {
+  try {
+    return fs.readFileSync(f + '/../package.json', 'utf-8');
+  } catch (e) {
+    return fs.readFileSync(f + '/../package.json.njk', 'utf-8');
+  }
+};
+
 module.exports = (file, data) => ({source}) => {
   if (!cache[file]) {
     cache[file] = true;
@@ -11,7 +19,7 @@ module.exports = (file, data) => ({source}) => {
       .dirSync(/src/, process.cwd())
       .filter(f => !f.match(/node_modules/));
     files.forEach(f => {
-      const meta = JSON.parse(fs.readFileSync(f + '/../package.json', 'utf-8'));
+      const meta = JSON.parse(readPackageJson(f));
       if (!(meta.name && meta.name.match(/^fusion-|^uber\/fusion-/))) {
         fs.writeFileSync(path.resolve(f + '/../' + file), data, 'utf-8');
       }
