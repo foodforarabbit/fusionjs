6e218ce95880b8b13a3a1a86929204c13c9d9027
Leo Horie <lhorie@uber.com>
2018-01-30 16:35:57 -0800

    bump csrf-react

--- private/fusion-migrate/src/codemods/0000-plugin-to-di-csrf-protection.js
+++ private/fusion-migrate/src/codemods/0000-plugin-to-di-csrf-protection.js
@@ -3,7 +3,7 @@ const bump = require('../utils/bump-version');
 
 module.exports = compose(
   bump('fusion-plugin-csrf-protection', '0.3.3'),
-  bump('fusion-plugin-csrf-protection-react', '0.3.2'),
+  bump('fusion-plugin-csrf-protection-react', '0.3.4'),
   ({source}) => {
     return source.replace(
       `import CsrfProtectionPlugin from 'fusion-plugin-csrf-protection-react';`,
