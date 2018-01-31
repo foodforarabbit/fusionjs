1f2033baacb91d8687b6a1cb68cffce5ee44d44b
Leo Horie <lhorie@uber.com>
2018-01-30 17:15:05 -0800

    fix codemod for events-adapter

--- private/fusion-migrate/src/codemods/0000-plugin-to-di-events-adapter.js
+++ private/fusion-migrate/src/codemods/0000-plugin-to-di-events-adapter.js
@@ -4,5 +4,21 @@ const migrate = require('../utils/plugin-to-di-standalone');
 
 module.exports = compose(
   bump('@uber/fusion-plugin-events-adapter', '0.4.1'),
-  migrate('EventsAdapterPlugin')
+  ({source}) =>
+    source.replace(
+      `import EventsAdapterPlugin from '@uber/fusion-plugin-events-adapter';`,
+      `import EventsAdapter, {EventsAdapterToken} from '@uber/fusion-plugin-events-adapter';
+import AnalyticsSessionPlugin, {
+  UberWebEventCookie,
+  AnalyticsCookieTypeToken,
+  AnalyticsSessionToken
+} from 'fusion-plugin-analytics-session';`
+    ),
+  ({source}) =>
+    source.replace(
+      `app.plugin(EventsAdapterPlugin, {UniversalEvents, config: {service}});`,
+      `app.register(EventsAdapterToken, EventsAdapter);
+  app.register(AnalyticsSessionToken, AnalyticsSessionPlugin);
+  app.register(AnalyticsCookieTypeToken, UberWebEventCookie);`
+    )
 );
