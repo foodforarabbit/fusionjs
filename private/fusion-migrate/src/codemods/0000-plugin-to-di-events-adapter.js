bfd8d21c9879711a4d0d8a5c7931c97943ad72ae
Leo Horie <lhorie@uber.com>
2018-01-31 16:36:02 -0800

    add missing import

--- private/fusion-migrate/src/codemods/0000-plugin-to-di-events-adapter.js
+++ private/fusion-migrate/src/codemods/0000-plugin-to-di-events-adapter.js
@@ -1,6 +1,6 @@
 const compose = require('../utils/compose');
+const add = require('../utils/add-package');
 const bump = require('../utils/bump-version');
-const migrate = require('../utils/plugin-to-di-standalone');
 
 module.exports = compose(
   bump('@uber/fusion-plugin-events-adapter', '0.4.2'),
