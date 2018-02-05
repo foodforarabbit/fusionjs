4267d902c74c0f3059928fae01bbdb31fcd0d86d
Leo Horie <lhorie@uber.com>
2018-02-05 12:08:32 -0800

    bump fusion-react-async

--- private/fusion-migrate/src/codemods/0000-fusion-core-greenkeeping.js
+++ private/fusion-migrate/src/codemods/0000-fusion-core-greenkeeping.js
@@ -6,6 +6,7 @@ module.exports = compose(
   bump('fusion-core', '0.3.5'),
   bump('fusion-cli', '0.3.2'),
   bump('fusion-react', '0.4.3'),
+  bump('fusion-react-async', '0.2.0'),
   bump('fusion-test-utils', '0.4.2'),
   add('fusion-tokens', '^0.0.6')
 );
