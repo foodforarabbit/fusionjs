9cc42737e2ca18a0770a267166c041dce83ab3df
Alexander Smith <amsmith@uber.com>
2018-02-06 12:44:11 -0800

    Remove dead browser code
    Reviewers: lhorie
    
    Reviewed By: lhorie
    
    Differential Revision: https://code.uberinternal.com/D1485739

--- private/fusion-plugin-auth-headers/src/__tests__/test.browser.js
+++ private/fusion-plugin-auth-headers/src/__tests__/test.browser.js
@@ -1,42 +1,8 @@
 import test from 'tape-cup';
 
-import App, {createPlugin} from 'fusion-core';
-import {createToken} from 'fusion-core';
-import {getSimulator} from 'fusion-test-utils';
-
 import AuthHeadersPlugin from '../browser';
 
-const MockPluginToken = createToken('test-plugin-token');
-function createTestFixture() {
-  const app = new App('content', el => el);
-  app.register(MockPluginToken, AuthHeadersPlugin);
-  return app;
-}
-
-test('exported as expected', t => {
-  t.ok(AuthHeadersPlugin, 'plugin defined as expected');
-  t.equal(typeof AuthHeadersPlugin, 'object', 'plugin is an object');
-  t.end();
-});
-
-test('arguments and service not supported on browser', t => {
-  const app = createTestFixture();
-
-  t.plan(2);
-  getSimulator(
-    app,
-    createPlugin({
-      deps: {authHeaders: MockPluginToken},
-      provides: deps => {
-        const {authHeaders} = deps;
-        t.ok(authHeaders, 'plugin defined as expected');
-        t.throws(
-          authHeaders.from,
-          'should throw if attempted to instantiate on browser'
-        );
-      },
-    })
-  );
-
+test('null exported as expected', t => {
+  t.equal(AuthHeadersPlugin, null, 'plugin null as expected');
   t.end();
 });
--- private/fusion-plugin-auth-headers/src/browser.js
+++ private/fusion-plugin-auth-headers/src/browser.js
@@ -1,22 +1,4 @@
 // @flow
 /* eslint-env browser */
 
-import {createPlugin} from 'fusion-core';
-import type {FusionPlugin} from 'fusion-core';
-
-type AuthHeadersService = {from: () => void};
-const service: AuthHeadersService = {
-  from() {
-    throw new Error('Cannot call AuthHeaders.from in the browser');
-  },
-};
-
-type AuthHeadersPluginType = FusionPlugin<*, AuthHeadersService>;
-const plugin: AuthHeadersPluginType =
-  // $FlowFixMe
-  __BROWSER__ &&
-  createPlugin({
-    provides: () => service,
-  });
-
-export default plugin;
+export default null;
