a4b4f4f122dfd0b55ad30f498f74b5d549826ac8
Alexander Smith <amsmith@uber.com>
2018-02-08 17:35:11 -0800

    Export 'null' from browser
    Reviewers: keving
    
    Reviewed By: keving
    
    Subscribers: keving
    
    Differential Revision: https://code.uberinternal.com/D1493599

--- private/fusion-plugin-secure-headers/src/browser.js
+++ private/fusion-plugin-secure-headers/src/browser.js
@@ -1,11 +1,2 @@
-import {createPlugin} from 'fusion-core';
-
 /* eslint-env browser */
-export default __BROWSER__ &&
-  createPlugin({
-    provides: () => ({
-      from() {
-        throw new Error('Cannot call SecureHeaders.from() in the browser');
-      },
-    }),
-  });
+export default null;
