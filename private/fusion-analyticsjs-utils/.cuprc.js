b28105979df117ad066eec3292c7dc7f58328212
Giancarlo Anemone <ganemone@uber.com>
2018-02-09 16:36:26 -0800

    Update deps

--- private/fusion-analyticsjs-utils/.cuprc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  babel: {
-    plugins: process.env.NODE_ENV === 'test' ? [require.resolve('babel-plugin-istanbul')] : [require.resolve('babel-plugin-transform-flow-strip-types')],
-  },
-};
