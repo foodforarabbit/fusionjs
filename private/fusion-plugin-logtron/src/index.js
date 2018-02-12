7d5e73d56820d03a37e2b4241d1b79d448186821
Giancarlo Anemone <ganemone@uber.com>
2018-02-12 11:54:18 -0800

    Fix flowtypes

--- private/fusion-plugin-logtron/src/index.js
+++ private/fusion-plugin-logtron/src/index.js
@@ -1,8 +1,10 @@
+// @flow
 import ServerLogger, {BackendsToken, TeamToken} from './server';
 import BrowserLogger from './browser';
 
 export {default as mock} from './mock';
 
+declare var __NODE__: Boolean;
 export default (__NODE__ ? ServerLogger : BrowserLogger);
 
 export const LogtronBackendsToken = BackendsToken;
