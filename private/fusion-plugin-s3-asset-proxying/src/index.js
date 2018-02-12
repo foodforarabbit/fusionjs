2f2fe4880005d2c42ae06a129066142db3a1fa57
Giancarlo Anemone <ganemone@uber.com>
2018-02-12 11:54:29 -0800

    Fix flowtypes

--- private/fusion-plugin-s3-asset-proxying/src/index.js
+++ private/fusion-plugin-s3-asset-proxying/src/index.js
@@ -1,4 +1,6 @@
+// @flow
 import ServerClient from './server.js';
 import BrowserClient from './browser.js';
 
+declare var __NODE__: Boolean;
 export default (__NODE__ ? ServerClient : BrowserClient);
