b9a4d04bfbda03c7e254c5fa0fe43f8930302800
Giancarlo Anemone <ganemone@uber.com>
2018-02-12 11:54:00 -0800

    Fix flowtypes

--- private/fusion-plugin-analytics-session/src/index.js
+++ private/fusion-plugin-analytics-session/src/index.js
@@ -1,8 +1,10 @@
+// @flow
 /* eslint-env browser */
 import server from './server';
 import browser from './browser';
 import UberWebEventCookie from './cookie-types/uber-web-event';
 
+declare var __NODE__: Boolean;
 export default (__NODE__ ? server : browser);
 
 export {UberWebEventCookie};
