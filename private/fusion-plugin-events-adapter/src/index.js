8c11c51cb06c1a9e2736cc8d8a25dd6bbac97abc
Giancarlo Anemone <ganemone@uber.com>
2018-02-12 11:54:08 -0800

    Fix flowtypes

--- private/fusion-plugin-events-adapter/src/index.js
+++ private/fusion-plugin-events-adapter/src/index.js
@@ -1,7 +1,9 @@
+// @flow
 /* eslint-env browser */
 import server from './server';
 import browser from './browser';
 
 export {EventsAdapterToken, EventsAdapterAnalyticsToken} from './tokens';
 
+declare var __NODE__: Boolean;
 export default (__NODE__ ? server : browser);
