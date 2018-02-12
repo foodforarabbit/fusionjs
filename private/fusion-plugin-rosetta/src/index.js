0a8dd499191965c39443d92511dcd0784f267feb
Giancarlo Anemone <ganemone@uber.com>
2018-02-12 11:54:27 -0800

    Fix flowtypes

--- private/fusion-plugin-rosetta/src/index.js
+++ private/fusion-plugin-rosetta/src/index.js
@@ -1,5 +1,7 @@
+// @flow
 import server, {ConfigToken} from './server';
 import browser from './browser';
 
+declare var __NODE__: Boolean;
 export default (__NODE__ ? server : browser);
 export const RosettaConfigToken = __NODE__ && ConfigToken;
