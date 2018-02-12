adb568c2694c0e846ba7e648192e65dc5f08f8d9
Giancarlo Anemone <ganemone@uber.com>
2018-02-12 11:54:14 -0800

    Fix flowtypes

--- private/fusion-plugin-google-analytics/src/index.js
+++ private/fusion-plugin-google-analytics/src/index.js
@@ -1,8 +1,10 @@
+// @flow
 /* eslint-env browser */
 import {createToken} from 'fusion-core';
 import server from './server';
 import browser from './browser';
 
+declare var __NODE__: Boolean;
 export default (__NODE__ ? server : browser);
 export {GoogleAnalyticsConfigToken} from './browser';
 export const GoogleAnalyticsToken = createToken('Google Analytics');
