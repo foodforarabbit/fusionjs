bc9b02e0b1e2d1ca1da62f2d219c34e0094382ba
Giancarlo Anemone <ganemone@uber.com>
2018-02-12 11:54:10 -0800

    Fix flowtypes

--- private/fusion-plugin-flipr/src/index.js
+++ private/fusion-plugin-flipr/src/index.js
@@ -1,6 +1,8 @@
+// @flow
 import server from './server';
 import browser from './browser';
 
 export {FliprToken, FliprClientToken, FliprConfigToken} from './tokens';
 
+declare var __NODE__: Boolean;
 export default (__NODE__ ? server : browser);
