17aedafa56d5abd0f56045e3a01f2ef63dfa349f
Giancarlo Anemone <ganemone@uber.com>
2018-02-12 11:54:12 -0800

    Fix flowtypes

--- private/fusion-plugin-galileo/src/index.js
+++ private/fusion-plugin-galileo/src/index.js
@@ -1,8 +1,10 @@
+// @flow
 // Main export file
 import {createToken} from 'fusion-core';
 import server, {ConfigToken} from './server';
 import browser from './browser';
 
+declare var __NODE__: Boolean;
 export default (__NODE__ ? server : browser);
 export const GalileoConfigToken = __NODE__ && ConfigToken;
 export const GalileoToken = createToken('GalileoToken');
