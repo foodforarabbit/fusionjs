40c37b384afa7c8e18765a8aba03de2c00072340
Giancarlo Anemone <ganemone@uber.com>
2018-02-12 11:54:31 -0800

    Fix flowtypes

--- private/fusion-plugin-secrets/src/index.js
+++ private/fusion-plugin-secrets/src/index.js
@@ -1,5 +1,7 @@
+// @flow
 import ServerClient from './server.js';
 import BrowserClient from './browser.js';
 
+declare var __NODE__: Boolean;
 export default (__NODE__ ? ServerClient : BrowserClient);
 export {SecretsToken, DevSecretsToken, SecretsLocationToken} from './tokens';
