67a90347d54e9eb1c1ceaa1176dc2c2c2d2555fd
Giancarlo Anemone <ganemone@uber.com>
2018-02-12 11:54:36 -0800

    Fix flowtypes

--- private/fusion-plugin-tchannel/src/index.js
+++ private/fusion-plugin-tchannel/src/index.js
@@ -1,5 +1,7 @@
+// @flow
 import ServerLogger from './server';
 import BrowserLogger from './browser';
 
+declare var __NODE__: Boolean;
 export default (__NODE__ ? ServerLogger : BrowserLogger);
 export * from './tokens';
