b66573ca45521c9889cfa326be4e5bea6d24b429
Giancarlo Anemone <ganemone@uber.com>
2018-02-12 11:54:42 -0800

    Fix flowtypes

--- private/fusion-plugin-error-handling/src/index.js
+++ private/fusion-plugin-error-handling/src/index.js
@@ -1,4 +1,6 @@
+// @flow
 import UberServerErrorHandler from './server';
 import UberBrowserErrorHandler from './browser';
 
+declare var __NODE__: Boolean;
 export default (__NODE__ ? UberServerErrorHandler : UberBrowserErrorHandler);
