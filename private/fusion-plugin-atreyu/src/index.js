71d9248d19d988dc992827a85673ba25f260723b
Giancarlo Anemone <ganemone@uber.com>
2018-02-12 11:54:03 -0800

    Fix flowtypes

--- private/fusion-plugin-atreyu/src/index.js
+++ private/fusion-plugin-atreyu/src/index.js
@@ -1,5 +1,7 @@
+// @flow
 import ServerAtreyu from './server';
 import BrowserAtreyu from './browser';
 
+declare var __NODE__: Boolean;
 export default (__NODE__ ? ServerAtreyu : BrowserAtreyu);
 export {AtreyuToken, AtreyuConfigToken, AtreyuOptionsToken} from './tokens';
