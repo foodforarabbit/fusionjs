f7568072aff77e6b89b53fed62315750c403567c
Giancarlo Anemone <ganemone@uber.com>
2018-02-12 11:54:16 -0800

    Fix flowtypes

--- private/fusion-plugin-heatpipe/src/index.js
+++ private/fusion-plugin-heatpipe/src/index.js
@@ -1,6 +1,8 @@
+// @flow
 /* eslint-env browser */
 import server from './server';
 import browser from './browser';
 
+declare var __NODE__: Boolean;
 export default (__NODE__ ? server : browser);
 export {HeatpipeToken, HeatpipeConfigToken} from './tokens';
