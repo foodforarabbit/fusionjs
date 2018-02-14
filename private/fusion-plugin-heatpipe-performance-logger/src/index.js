65a5edc262db78b8a1589575797af771392cc5c7
Angus Croll <angus@uber.com>
2018-02-14 15:13:22 -0800

    imake fake numbers float
    Summary: make fake numbers floats
    
    Differential Revision: https://code.uberinternal.com/D1506813

--- private/fusion-plugin-heatpipe-performance-logger/src/index.js
+++ private/fusion-plugin-heatpipe-performance-logger/src/index.js
@@ -32,20 +32,15 @@ function mapPerformanceDataToSchema(event) {
 
     const {navigation, resources: resourcesRaw, network: rawNetwork, memory, firstPaint, tags, appId} = event;
 
-    // const [url, qs] = tags.url ? tags.url.split('?') : [];
-    const navigationMeta = {
-      //   url,
-      //   qs
-    };
-
-    const app = {
-      name: appId,
-      external: true,
-    };
-
     navigation.redirectDuration = navigation.responseEnd - navigation.redirectStart;
+    // TEMP: to ensure liberal data types in schema
+    Object.keys(navigation)
+      .filter(prop => typeof navigation[prop] === 'number')
+      .forEach(prop => navigation[prop] = Math.floor(navigation[prop]) + 0.1);
+
 
     const resources = resourcesRaw.map(resource => {
+      resource.originalName = resource.name; // TODO:
       resource.initiator = resource.initiatorType;
       resource.resourceSize = resource.decodedBodySize;
       const {domComplete, fetchStart, resourceSize, transferSize} = resource;
@@ -53,33 +48,86 @@ function mapPerformanceDataToSchema(event) {
       resource.duration = resource.responseEnd - resource.fetchStart;
       // find a better way to do this...
       resource.fromCache = resourceSize / transferSize > 2;
+
+      // TEMP: to ensure liberal data types in schema
+      Object.keys(resource)
+        .filter(prop => typeof resource[prop] === 'number')
+        .forEach(prop => resource[prop] = Math.floor(resource[prop]) + 0.1);
+
       return resource;
     });
 
+    const app = {
+      name: appId,
+      version: 2.1, // TODO
+      external: true, // TODO
+    };
+
+    const context = {
+      // const [url, qs] = tags.url ? tags.url.split('?') : [];
+      url: 'https://browser-tests.com', // TODO
+      qs: 'ts=45', // TODO
+      referrer: 'lorem ipsum', // TODO
+      type: 'lorem ipsum', // TODO
+      loggedIn: false, // TODO
+    };
+
     const network = {
-      // region,
-      // country,
-      ...rawNetwork,
+      region: 'North America', // TODO
+      country: 'Canada', // TODO
+      ip: 462378647823, // TODO
+      downlink: 4.25, // TODO
+      effectiveType: "4g", // TODO
+      rtt: 50, // TODO
+      // ...rawNetwork,
+    };
+
+    const server = {
+      build: '762', // TODO
+      dc: 'SJC1', // TODO
+      env: 'lorem ipsum', // TODO
+      host: 'lorem ipsum', // TODO
+      node_version: 123.45, // TODO
     };
 
     const client = {
-      // locale,
-      // device,
-      // browser,
-      memory,
-      // serviceWorker,
+      locale: 'en-CA', // TODO
+      ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36', // TODO
+      device: {
+        manufacturer: 'Apple', // TODO
+        model: 'iPhone', // TODO
+        type: 'phone', // TODO
+        osType: 'iOS', // TODO
+        osVersion: '9.1', // TODO
+      },
+      browser: {
+        manufacturer: 'Apple', // TODO
+        name: 'Safari', // TODO
+        version: '9.1.1', // TODO
+        engine: 'WebKit', // TODO
+        engineVersion: '531.21.10' // TODO
+      },
+      memory: {
+        jsHeapSizeLimit: 2190000000, // TODO
+        totalJSHeapSize: 72200000, // TODO
+        usedJSHeapSize: 68000000, // TODO
+      },
+      serviceWorker: {
+        supported: false, // TODO
+        active: false, // TODO
+      }
     };
 
-    const server = {};
-
     const result = {
-      navigationMeta,
+      context,
       network,
       app,
       timings: {navigation, resources},
       client,
       server,
     };
+
+    console.log('••••••', JSON.stringify(result, null, 2));
 
     return result;
   }
