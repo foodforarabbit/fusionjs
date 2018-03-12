79e09ed49fc70e470372eba2f84f92339f7b2c20
Giancarlo Anemone <ganemone@uber.com>
2018-03-12 15:13:50 -0700

    Initial implementation of web-rpc plugin
    Reviewers: #web_platform, keving
    
    Reviewed By: #web_platform, keving
    
    Differential Revision: https://code.uberinternal.com/D1568571

---
+++ private/fusion-plugin-web-rpc-compat/src/index.js
@@ -0,0 +1,30 @@
+import {createPlugin} from 'fusion-core';
+import {RPCToken} from 'fusion-plugin-rpc';
+
+export default createPlugin({
+  deps: {
+    RPC: RPCToken,
+  },
+  provides({RPC}) {
+    return createStore => (...args) => {
+      const store = createStore(...args);
+      const baseRPC = RPC.from(store.ctx);
+      function rpc(rpcId, args, cb) {
+        return baseRPC
+          .request(rpcId, args)
+          .then(result => cb(null, result))
+          .catch(e => cb(e, e));
+      }
+      function dispatch(action) {
+        if (typeof action === 'function') {
+          return action(dispatch, store.getState, {rpc});
+        }
+        return store.dispatch(action);
+      }
+      return {
+        ...store,
+        dispatch,
+      };
+    };
+  },
+});
