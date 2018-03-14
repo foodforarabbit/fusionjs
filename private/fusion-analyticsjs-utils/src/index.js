7cd2f68b4826ecf462471183bc51ed1302fdf7c0
Dennis Lin <dennis.lin@uber.com>
2018-03-13 17:55:50 -0700

    fix exports typo

--- private/fusion-analyticsjs-utils/src/index.js
+++ private/fusion-analyticsjs-utils/src/index.js
@@ -3,7 +3,14 @@ import cleanData from './utilities/clean-data';
 import compactArray from './utilities/compact-array';
 import dataQueue from './utilities/data-queue';
 import isEmpty from './utilities/is-empty';
-import pageData from './utilities/page-data';
+import getPageData from './utilities/page-data';
 import stringifyData from './utilities/stringify-data';
 
-export {cleanData, compactArray, dataQueue, isEmpty, pageData, stringifyData};
+export {
+  cleanData,
+  compactArray,
+  dataQueue,
+  isEmpty,
+  getPageData,
+  stringifyData,
+};
