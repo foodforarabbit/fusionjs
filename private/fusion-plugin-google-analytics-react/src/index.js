ff669b5da9456318359d971f2dd2e09529c8bf5d
Dennis Lin <dennis.lin@uber.com>
2018-03-15 08:46:41 -0700

    initial implementation
    Test Plan: yarn test
    
    Reviewers: sblotner, nadiia, ganemone
    
    Reviewed By: ganemone
    
    Subscribers: nadiia
    
    Differential Revision: https://code.uberinternal.com/D1578235

---
+++ private/fusion-plugin-google-analytics-react/src/index.js
@@ -0,0 +1,17 @@
+// @flow
+/* eslint-env browser */
+import plugin from './plugin';
+import withGoogleAnalytics from './hoc';
+
+import {
+  GoogleAnalyticsToken,
+  GoogleAnalyticsConfigToken,
+} from '@uber/fusion-plugin-google-analytics';
+
+export {
+  GoogleAnalyticsToken,
+  GoogleAnalyticsConfigToken,
+};
+
+export default plugin;
+export {withGoogleAnalytics};
---
+++ private/fusion-plugin-google-analytics-react/src/plugin.js
@@ -0,0 +1,4 @@
+import GoogleAnalyticsPlugin from '@uber/fusion-plugin-google-analytics';
+import {ProviderPlugin} from 'fusion-react';
+
+export default ProviderPlugin.create('googleAnalytics', GoogleAnalyticsPlugin);
