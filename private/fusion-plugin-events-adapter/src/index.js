e9f544ebf385a7576772d923865ddde8cfc49dc0
Nicholas Spyropoulos <nickspy@uber.com>
2018-03-20 11:19:08 -0700

    make sure meta mapper is exported
    Reviewers: dennis.lin
    
    Reviewed By: dennis.lin
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D1589857

--- private/fusion-plugin-events-adapter/src/index.js
+++ private/fusion-plugin-events-adapter/src/index.js
@@ -4,6 +4,7 @@ import server from './server';
 import browser from './browser';
 
 export {EventsAdapterToken, EventsAdapterAnalyticsToken} from './tokens';
+export {webEventsMetaMapper} from './browser';
 
 declare var __NODE__: Boolean;
 export default (__NODE__ ? server : browser);
