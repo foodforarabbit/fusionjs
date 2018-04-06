37d96e46fb368c30f220fd4244bfa12d757c43c8
Giancarlo Anemone <ganemone@uber.com>
2018-04-06 09:55:55 -0700

    Add util for getting js files
    Reviewers: #web_platform, dennis.lin
    
    Reviewed By: #web_platform, dennis.lin
    
    Differential Revision: https://code.uberinternal.com/D1636313

---
+++ private/fusion-migrate/src/utils/__tests__/get-js-files.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const path = require('path');
+const getJSFiles = require('../get-js-files.js');
+
+test('get js files', async () => {
+  const files = await getJSFiles(
+    path.join(__dirname, '../../__fixtures__/js-files-fixture')
+  );
+  assert.equal(files.length, 4, 'only finds js files');
+  assert.ok(files.includes('top.js'), 'includes top level js files');
+  assert.ok(
+    files.includes('src/main.js', 'includes js files nested in directories')
+  );
+  assert.ok(files.includes('top.jsx'), 'includes top level js files');
+  assert.ok(
+    files.includes('src/main.jsx', 'includes js files nested in directories')
+  );
+});
