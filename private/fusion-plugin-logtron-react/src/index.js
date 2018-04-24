094c6c7917775530008398ad8b4e48adf40bdb70
Rasmus Prentow <prentow@uber.com>
2018-04-24 11:45:46 +0200

    Bump fusion-plugin-logtron and expose TransformsToken
    Summary: Bumps the version of `fusion-plugin-logtron` from 1.0.0 to 1.2.0 and expose `LogtronTransformsToken`.
    
    Reviewers: #web_platform, amsmith
    
    Reviewed By: #web_platform, amsmith
    
    Differential Revision: https://code.uberinternal.com/D1684117

--- private/fusion-plugin-logtron-react/src/index.js
+++ private/fusion-plugin-logtron-react/src/index.js
@@ -5,12 +5,13 @@
 import {
   LogtronBackendsToken,
   LogtronTeamToken,
+  LogtronTransformsToken,
 } from '@uber/fusion-plugin-logtron';
 
 import plugin from './plugin';
 import withLogger from './hoc';
 
-export {LogtronBackendsToken, LogtronTeamToken};
+export {LogtronBackendsToken, LogtronTeamToken, LogtronTransformsToken};
 
 export default plugin;
 export {withLogger};
