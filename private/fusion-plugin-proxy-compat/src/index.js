9c20ca278fc5677b97f5ee9787118e62c7c1c869
Giancarlo Anemone <ganemone@uber.com>
2018-04-24 15:55:54 -0700

    Add missing export

--- private/fusion-plugin-proxy-compat/src/index.js
+++ private/fusion-plugin-proxy-compat/src/index.js
@@ -1,5 +1,6 @@
-import plugin from './plugin.js';
+import plugin, {ProxyConfigToken} from './plugin.js';
 import decider from './decider.js';
 
 export default __NODE__ && plugin;
 export const ProxySSRDecider = __NODE__ && decider;
+export {ProxyConfigToken};
