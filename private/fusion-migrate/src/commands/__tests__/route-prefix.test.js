e2ca95aa279e14e853af837fcf3ca0322354f962
Giancarlo Anemone <ganemone@uber.com>
2018-05-09 11:01:26 -0400

    Add command for adding route prefix into package.json scripts
    Reviewers: #web_platform, keving
    
    Reviewed By: #web_platform, keving
    
    Subscribers: keving
    
    Differential Revision: https://code.uberinternal.com/D1727555

---
+++ private/fusion-migrate/src/commands/__tests__/route-prefix.test.js
@@ -0,0 +1,25 @@
+const {promisify} = require('util');
+const fs = require('fs');
+const path = require('path');
+
+const ncp = promisify(require('ncp'));
+const tmp = require('tmp');
+const routePrefix = require('../route-prefix.js');
+
+const srcFixtureDir = path.join(
+  __dirname,
+  '../../__fixtures__/src-package-fixture/'
+);
+test('routePrefix', async () => {
+  const dir = tmp.dirSync().name;
+  await ncp(srcFixtureDir, dir);
+  await routePrefix({
+    destDir: dir,
+    routePrefix: 'test',
+  });
+  const finalPackage = JSON.parse(
+    fs.readFileSync(path.join(dir, 'package.json'))
+  );
+  expect(finalPackage.scripts.dev).toEqual('ROUTE_PREFIX=test SVC_ID=tmp dev');
+  expect(finalPackage.scripts.start).toEqual('ROUTE_PREFIX=test start');
+});
---
+++ private/fusion-migrate/src/commands/route-prefix.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function updateDeps({destDir, routePrefix}) {
+  const destPackagePath = path.join(destDir, 'package.json');
+  const destPackage = JSON.parse(fs.readFileSync(destPackagePath).toString());
+
+  const oldDev = destPackage.scripts.dev;
+  const oldStart = destPackage.scripts.start;
+
+  destPackage.scripts.dev = `ROUTE_PREFIX=${routePrefix} ${oldDev}`;
+  destPackage.scripts.start = `ROUTE_PREFIX=${routePrefix} ${oldStart}`;
+
+  fs.writeFileSync(destPackagePath, JSON.stringify(destPackage, null, 2));
+};
