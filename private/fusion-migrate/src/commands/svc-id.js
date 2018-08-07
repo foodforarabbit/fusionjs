e6437690dd562a9a9150f4a11f3c91c901e78f82
Leo Horie <lhorie@uber.com>
2018-08-07 10:08:16 -0700

    Don't abort migration if there's no dev script
    Reviewers: dennis.lin
    
    Reviewed By: dennis.lin
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D1964081

--- private/fusion-migrate/src/commands/svc-id.js
+++ private/fusion-migrate/src/commands/svc-id.js
@@ -5,7 +5,9 @@ module.exports = function updateDeps({destDir, svcId}) {
   const destPackagePath = path.join(destDir, 'package.json');
   const destPackage = JSON.parse(fs.readFileSync(destPackagePath).toString());
 
-  destPackage.scripts.dev = destPackage.scripts.dev.replace('tmp', svcId);
+  if (destPackage.scripts && destPackage.scripts.dev) {
+    destPackage.scripts.dev = destPackage.scripts.dev.replace('tmp', svcId);
+  }
 
   fs.writeFileSync(destPackagePath, JSON.stringify(destPackage, null, 2));
 };
