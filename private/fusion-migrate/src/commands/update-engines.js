c669bbb9c3e1b0d42037d092a86850a6e98fc7fc
Giancarlo Anemone <ganemone@uber.com>
2018-11-29 12:50:13 -0500

    Hardcode node/yarn versions
    We need to hardcode the node version until we support node 10.
    We also need to hardcode the yarn version because of a bug.
    See: https://github.com/yarnpkg/yarn/issues/6608

--- private/fusion-migrate/src/commands/update-engines.js
+++ private/fusion-migrate/src/commands/update-engines.js
@@ -1,7 +1,7 @@
 const {promisify} = require('util');
 const fs = require('fs');
 
-var execSync = require('child_process').execSync;
+// var execSync = require('child_process').execSync;
 
 const writeFile = promisify(fs.writeFile);
 const path = require('path');
@@ -18,36 +18,40 @@ module.exports = function updateEngines({destDir}) {
 };
 
 function fetchRequiredNodeVersion() {
+  // Hard code node version until we support node 10
+  return '8.12.0';
   // Borrowed from: https://github.com/tj/n/blob/master/bin/n#L715
-  var latestLTSName = execSync(
-    'curl https://nodejs.org/dist/ ' +
-      '| egrep "</a>" ' +
-      "| egrep -o 'latest-[a-z]{2,}' " +
-      '| sort ' +
-      '| tail -n1',
-    {stdio: 'pipe'}
-  )
-    .toString()
-    .trim();
+  // var latestLTSName = execSync(
+  //   'curl https://nodejs.org/dist/ ' +
+  //     '| egrep "</a>" ' +
+  //     "| egrep -o 'latest-[a-z]{2,}' " +
+  //     '| sort ' +
+  //     '| tail -n1',
+  //   {stdio: 'pipe'}
+  // )
+  //   .toString()
+  //   .trim();
 
-  var ltsVersion = execSync(
-    'curl https://nodejs.org/dist/' +
-      latestLTSName +
-      '/ ' +
-      '| egrep "</a>" ' +
-      "| egrep -o '[0-9]+\\.[0-9]+\\.[0-9]+' " +
-      '| head -n1',
-    {stdio: 'pipe'}
-  )
-    .toString()
-    .trim();
+  // var ltsVersion = execSync(
+  //   'curl https://nodejs.org/dist/' +
+  //     latestLTSName +
+  //     '/ ' +
+  //     '| egrep "</a>" ' +
+  //     "| egrep -o '[0-9]+\\.[0-9]+\\.[0-9]+' " +
+  //     '| head -n1',
+  //   {stdio: 'pipe'}
+  // )
+  //   .toString()
+  //   .trim();
 
-  return ltsVersion;
+  // return ltsVersion;
 }
 
 function fetchRequiredYarnVersion() {
-  var latestYarnVersion = JSON.parse(
-    execSync('npm info yarn --json', {stdio: 'pipe'})
-  )['dist-tags'].latest;
-  return latestYarnVersion;
+  // See https://github.com/yarnpkg/yarn/issues/6608
+  return '1.9.2';
+  // var latestYarnVersion = JSON.parse(
+  //   execSync('npm info yarn --json', {stdio: 'pipe'})
+  // )['dist-tags'].latest;
+  // return latestYarnVersion;
 }
