c08460dde357f16d909ddece02995a92c1def330
Chris Deacy <deacy@uber.com>
2018-12-03 15:03:04 -0800

    upgrading deps; fixing flow errors
    Reviewers: lhorie
    
    Reviewed By: lhorie
    
    Subscribers: lhorie, ucdbot
    
    Tags: #bugfix_fusionreleaseprocess
    
    Differential Revision: https://code.uberinternal.com/D2319537

--- private/fusion-plugin-logtron-react/src/plugin.js
+++ private/fusion-plugin-logtron-react/src/plugin.js
@@ -2,5 +2,10 @@
 
 import LogtronPlugin from '@uber/fusion-plugin-logtron';
 import {ProviderPlugin} from 'fusion-react';
+import type {Logger as LoggerType} from 'fusion-tokens';
+import type {LogtronDepsType} from '@uber/fusion-plugin-logtron';
 
-export default ProviderPlugin.create('logger', LogtronPlugin);
+export default ProviderPlugin.create<LogtronDepsType, LoggerType>(
+  'logger',
+  LogtronPlugin
+);
