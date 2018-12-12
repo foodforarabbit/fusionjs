79ff1775e929b320a6c667c4cdafc272db6cf1bc
Leo Horie <lhorie@uber.com>
2018-12-12 14:52:56 -0800

    Actually treeshake server code out of browser bundle
    Summary:
    The createPlugin call makes the server expression impure, thus it cannot be treeshaken
    
    This diff ensures that the createPlugin call happens inside a __NODE__ fence, allowing it to be treeshaken correctly
    
    Reviewers: keving
    
    Reviewed By: keving
    
    Subscribers: keving, ucdbot
    
    Differential Revision: https://code.uberinternal.com/D2354951

--- private/fusion-plugin-s3-asset-proxying/src/server.js
+++ private/fusion-plugin-s3-asset-proxying/src/server.js
@@ -16,114 +16,117 @@ const defaultViewerResponseHeaders = {
   'timing-allow-origin': '*',
 };
 
-const plugin = createPlugin<S3AssetProxyDepsType, S3AssetProxyType>({
-  deps: {
-    s3Config: S3ConfigToken.optional,
-    customViewerResponseHeaders: AssetProxyingResponseHeaderOverrides.optional,
-  },
-  provides: deps => {
-    const aws = require('aws-sdk');
-    const loadConfig = require('../s3-config');
-    const s3Config = deps.s3Config || loadConfig();
-    class S3Service {
-      notFoundCache: LRU;
-      config: S3ConfigType;
-      s3: aws.S3;
+export default ((__NODE__ &&
+  createPlugin<S3AssetProxyDepsType, S3AssetProxyType>({
+    deps: {
+      s3Config: S3ConfigToken.optional,
+      customViewerResponseHeaders:
+        AssetProxyingResponseHeaderOverrides.optional,
+    },
+    provides: deps => {
+      const aws = require('aws-sdk');
+      const loadConfig = require('../s3-config');
+      const s3Config = deps.s3Config || loadConfig();
+      class S3Service {
+        notFoundCache: LRU;
+        config: S3ConfigType;
+        s3: aws.S3;
 
-      constructor() {
-        this.notFoundCache = new LRU({
-          // store 404s
-          max: 500,
-        });
-        this.config = s3Config;
-        const {
-          accessKeyId,
-          secretAccessKey,
-          s3ForcePathStyle,
-          endpoint,
-        } = this.config;
-        this.s3 = new aws.S3({
-          accessKeyId,
-          secretAccessKey,
-          s3ForcePathStyle,
-          endpoint,
-        });
-      }
-    }
-    return new S3Service();
-  },
-  middleware: (deps, state) => {
-    return async function middleware(ctx, next) {
-      if (ctx.method !== 'HEAD' && ctx.method !== 'GET' && ctx.status !== 404) {
-        return next();
-      }
-
-      const customViewerResponseHeaders =
-        deps.customViewerResponseHeaders || {};
-
-      // Only proxy URLs starting with /_static
-      if (!ctx.path || !ctx.path.startsWith('/_static/')) {
-        return next();
-      }
-
-      const reqPath = ctx.path.substring(9);
-      const notFoundCache = state.notFoundCache;
-      if (notFoundCache.get(reqPath)) {
-        // If we know that a previous request to this asset has responded with a 404
-        // we can safely assume that it will not be available in S3 until the next
-        // service deploy.
-        return next();
-      }
-
-      const s3 = state.s3;
-      const {bucket, prefix} = state.config;
-
-      const {
-        s3Error,
-        statusCode,
-        headers: s3ResponseHeaders,
-        data,
-      } = await new Promise(resolve =>
-        s3.getObject(
-          {
-            Bucket: bucket,
-            Key: path.join(prefix, reqPath),
-          },
-          function(s3Error, data) {
-            let statusCode = this.httpResponse && this.httpResponse.statusCode;
-            const headers = this.httpResponse && this.httpResponse.headers;
-            return resolve({s3Error, statusCode, headers, data});
-          }
-        )
-      );
-
-      if (s3Error || statusCode !== 200) {
-        // Cache 404 responses. If these assets are requested by a browser
-        // and are not currently in S3, there is no way they can appear
-        // there unless a project is redeployed. We can safely cache
-        // the 404 responses from S3 in memory, and direct the static asset
-        // server to proceed to the application router instead.
-        // S3 sometimes returns 403 instead of 404
-        if (statusCode === 404 || statusCode === 403) {
-          notFoundCache.set(reqPath, true);
+        constructor() {
+          this.notFoundCache = new LRU({
+            // store 404s
+            max: 500,
+          });
+          this.config = s3Config;
+          const {
+            accessKeyId,
+            secretAccessKey,
+            s3ForcePathStyle,
+            endpoint,
+          } = this.config;
+          this.s3 = new aws.S3({
+            accessKeyId,
+            secretAccessKey,
+            s3ForcePathStyle,
+            endpoint,
+          });
         }
-      } else {
-        const responseHeaders = {
-          ...s3ResponseHeaders,
-          ...defaultViewerResponseHeaders,
-          ...customViewerResponseHeaders,
-        };
-        Object.keys(responseHeaders).forEach(h =>
-          ctx.set(h, responseHeaders[h])
-        );
-        ctx.status = statusCode;
-        ctx.body = data.Body;
       }
-      return next();
-    };
-  },
-});
-export default ((__NODE__ && plugin: any): FusionPlugin<
-  S3AssetProxyDepsType,
-  S3AssetProxyType
->);
+      return new S3Service();
+    },
+    middleware: (deps, state) => {
+      return async function middleware(ctx, next) {
+        if (
+          ctx.method !== 'HEAD' &&
+          ctx.method !== 'GET' &&
+          ctx.status !== 404
+        ) {
+          return next();
+        }
+
+        const customViewerResponseHeaders =
+          deps.customViewerResponseHeaders || {};
+
+        // Only proxy URLs starting with /_static
+        if (!ctx.path || !ctx.path.startsWith('/_static/')) {
+          return next();
+        }
+
+        const reqPath = ctx.path.substring(9);
+        const notFoundCache = state.notFoundCache;
+        if (notFoundCache.get(reqPath)) {
+          // If we know that a previous request to this asset has responded with a 404
+          // we can safely assume that it will not be available in S3 until the next
+          // service deploy.
+          return next();
+        }
+
+        const s3 = state.s3;
+        const {bucket, prefix} = state.config;
+
+        const {
+          s3Error,
+          statusCode,
+          headers: s3ResponseHeaders,
+          data,
+        } = await new Promise(resolve =>
+          s3.getObject(
+            {
+              Bucket: bucket,
+              Key: path.join(prefix, reqPath),
+            },
+            function(s3Error, data) {
+              let statusCode =
+                this.httpResponse && this.httpResponse.statusCode;
+              const headers = this.httpResponse && this.httpResponse.headers;
+              return resolve({s3Error, statusCode, headers, data});
+            }
+          )
+        );
+
+        if (s3Error || statusCode !== 200) {
+          // Cache 404 responses. If these assets are requested by a browser
+          // and are not currently in S3, there is no way they can appear
+          // there unless a project is redeployed. We can safely cache
+          // the 404 responses from S3 in memory, and direct the static asset
+          // server to proceed to the application router instead.
+          // S3 sometimes returns 403 instead of 404
+          if (statusCode === 404 || statusCode === 403) {
+            notFoundCache.set(reqPath, true);
+          }
+        } else {
+          const responseHeaders = {
+            ...s3ResponseHeaders,
+            ...defaultViewerResponseHeaders,
+            ...customViewerResponseHeaders,
+          };
+          Object.keys(responseHeaders).forEach(h =>
+            ctx.set(h, responseHeaders[h])
+          );
+          ctx.status = statusCode;
+          ctx.body = data.Body;
+        }
+        return next();
+      };
+    },
+  }): any): FusionPlugin<S3AssetProxyDepsType, S3AssetProxyType>);
