7fc05f0b59651ea7daa12a4fc6df456c864e32b8
Giancarlo Anemone <ganemone@uber.com>
2019-01-23 14:31:27 -0500

    Re-Export mock
    Summary: Exports the mock from the underlying plugin
    
    Reviewers: #web_platform, amsmith
    
    Reviewed By: #web_platform, amsmith
    
    Subscribers: ucdbot
    
    Differential Revision: https://code.uberinternal.com/D2439545

--- private/fusion-plugin-m3-react/src/index.js
+++ private/fusion-plugin-m3-react/src/index.js
@@ -3,7 +3,12 @@
 import plugin from './plugin';
 import withM3 from './hoc';
 
-export {M3Token, M3ClientToken, CommonTagsToken} from '@uber/fusion-plugin-m3';
+export {
+  M3Token,
+  M3ClientToken,
+  CommonTagsToken,
+  mock,
+} from '@uber/fusion-plugin-m3';
 
 export default plugin;
 export {withM3};
