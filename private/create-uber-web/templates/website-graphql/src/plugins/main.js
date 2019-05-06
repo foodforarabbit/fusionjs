bd4fa2128dda4272577685b65e1388758cfc5c45
Giancarlo Anemone <ganemone@uber.com>
2019-05-06 12:53:28 -0400

    Only register health check plugin on the server
    Summary:
    Fixes issue where the health check plugin was
    being registered in the browser
    
    Reviewers: #web_platform, keving
    
    Reviewed By: #web_platform, keving
    
    Subscribers: jenkins, keving
    
    Differential Revision: https://code.uberinternal.com/D2798119

--- private/create-uber-web/templates/website-graphql/src/plugins/main.js
+++ private/create-uber-web/templates/website-graphql/src/plugins/main.js
@@ -3,6 +3,8 @@ import type FusionApp from 'fusion-core';
 import HealthPlugin from './health';
 
 export default function initUserPlugins(app: FusionApp) {
-  app.middleware(HealthPlugin);
+  if (__NODE__) {
+    app.middleware(HealthPlugin);
+  }
   return app;
 }
