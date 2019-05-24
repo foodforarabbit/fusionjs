f0db3f76c8688118483875b44837115081531574
Leo Horie <lhorie@uber.com>
2019-05-24 13:35:44 -0700

    [create-uber-web] Fix yarn version parsing in upgrade command (#74)

--- private/create-uber-web/src/utils/get-latest-version.js
+++ private/create-uber-web/src/utils/get-latest-version.js
@@ -30,8 +30,8 @@ export async function getLatestVersion(
   if (strategy === 'edge') {
     return (
       cache[dep] ||
-      exec(`yarn info ${dep} versions --json`).then(list => {
-        cache[dep] = `^${JSON.parse(list).pop()}`;
+      exec(`yarn info ${dep} versions --json 2>/dev/null`).then(list => {
+        cache[dep] = `^${JSON.parse(list).data.pop()}`;
         return cache[dep];
       })
     );
@@ -44,8 +44,8 @@ export async function getLatestVersion(
   } else {
     return (
       cache[dep] ||
-      exec(`yarn info ${dep} version`).then(v => {
-        cache[dep] = `^${v}`;
+      exec(`yarn info ${dep} version --json 2>/dev/null`).then(v => {
+        cache[dep] = `^${JSON.parse(v).data}`;
         return cache[dep];
       })
     );
