91a9d3f73b130a48e265d16a89b6d6ed3cfbc51f
Giancarlo Anemone <ganemone@uber.com>
2019-06-25 17:05:31 -0400

    Fix scaffold util in fusion-migrate
    https://github.com/uber/fusionjs/pull/218

--- private/fusion-migrate/src/utils/scaffold.js
+++ private/fusion-migrate/src/utils/scaffold.js
@@ -3,24 +3,7 @@ const tmp = require('tmp');
 const execa = require('execa');
 
 module.exports = async function scaffold(options = {}) {
-  const destClonePath = tmp.dirSync().name;
   const dirname = tmp.dirSync().name;
-  const project = 'tmp';
-  options = {
-    repo: {
-      remote:
-        'gitolite@code.uber.internal:web/uber-web-template-fusion-website',
-      name: 'remote-template-website',
-    },
-    destClonePath,
-    dirname,
-    project,
-    // TODO: do we care about this?
-    websiteType: 'internal',
-    ...options,
-  };
-  await execa.shell(
-    `git clone gitolite@code.uber.internal:web/create-uber-web ${dirname}`
-  );
-  return path.join(options.dirname, 'templates/website');
+  await execa.shell(`git clone git@github.com:uber/fusionjs.git ${dirname}`);
+  return path.join(dirname, 'private/create-uber-web/templates/website');
 };
