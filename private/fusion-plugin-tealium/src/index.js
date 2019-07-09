e300c3ab479b025f7868034eaf156da730ddf59a
Leyan Lo <leyanlo@gmail.com>
2019-07-09 13:59:15 -0700

    Export fusion-plugin-tealium types
    https://github.com/uber/fusionjs/pull/163
    
    Co-authored-by: Leyan Lo <leyan@uber.com>

--- private/fusion-plugin-tealium/src/index.js
+++ private/fusion-plugin-tealium/src/index.js
@@ -4,6 +4,7 @@ import server from './server';
 import browser from './browser';
 
 export {TealiumToken, TealiumConfigToken} from './tokens';
+export type {TealiumConfigType, TealiumType, TealiumDepsType} from './types';
 
 declare var __NODE__: Boolean;
 export default __NODE__ ? server : browser;
