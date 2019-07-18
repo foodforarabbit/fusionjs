ed958fbfefb4feb7ab2585b680492948f8b5e29e
Harshit Mittal <hmit@users.noreply.github.com>
2019-07-18 15:46:44 -0700

    Update default healthline/sentry dsn for new zone turnup (#306)
    * update default healthline/sentry dsn for new zone turnup
    
    * update more templates, remove unused code

--- private/create-uber-web/templates/website-graphql/src/config/sentry.js
+++ private/create-uber-web/templates/website-graphql/src/config/sentry.js
@@ -1,6 +1,4 @@
 // @flow
-const sentryEnv = __NODE__ ? process.env.UBER_DATACENTER || 'local' : 'local';
-
 export default {
-  id: `http://uber:uber@healthline-production.${sentryEnv}.uber.internal/{{name}}`,
+  id: `http://uber:uber@localhost:16921/{{name}}`,
 };
