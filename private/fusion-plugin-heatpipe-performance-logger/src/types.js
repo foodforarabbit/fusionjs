2defd3be06597c34462dabac352e2ba44f5ce3d0
angus croll <angus@uber.com>
2019-07-23 16:33:28 -0700

    Remove fusion-plugin-heatpipe-performance-logger (#328)
    * wip
    
    * wip

--- private/fusion-plugin-heatpipe-performance-logger/src/types.js
+++
@@ -1,7 +0,0 @@
-// @flow
-import {UniversalEventsToken} from 'fusion-plugin-universal-events';
-
-export type HeatpipePerformanceLoggerDepsType = {
-  emitter: typeof UniversalEventsToken,
-};
-export type HeatpipePerformanceLoggerType = typeof undefined;
