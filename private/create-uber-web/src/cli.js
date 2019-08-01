562ec6c45de0a583ebc2c4e386f28844e7a5fd9b
Mickey Burks <mburks@uber.com>
2019-08-01 13:36:42 -0700

    Enable create-uber-web canary version
    https://github.com/uber/fusionjs/pull/366

--- private/create-uber-web/src/cli.js
+++ private/create-uber-web/src/cli.js
@@ -3,6 +3,7 @@
 /* @flow */
 
 import proc from 'child_process';
+import {promptChoice} from './utils/prompt-choice.js';
 
 process.on('unhandledRejection', e => {
   throw e;
@@ -14,18 +15,31 @@ const latest = proc
   .trim();
 const version = require('../package.json').version;
 
-if (latest !== version) {
-  // eslint-disable-next-line no-console
-  console.log(`Upgrading uber-web from ${version} to ${latest}`);
-  // do a clean install because upgrades can break with stupid errors e.g. EISGIT.
-  proc.execSync(`
+(async function() {
+  let keepCanary = false;
+  if (version.includes('canary')) {
+    keepCanary = await promptChoice(
+      `Canary version ${version} is installed, keep this version?`,
+      {
+        'Yes - use current canary version': true,
+        'No - install latest version': false,
+      }
+    );
+  }
+
+  if (latest !== version && !keepCanary) {
+    // eslint-disable-next-line no-console
+    console.log(`Upgrading uber-web from ${version} to ${latest}`);
+    // do a clean install because upgrades can break with stupid errors e.g. EISGIT.
+    proc.execSync(`
     yarn global remove @uber/create-uber-web || true;
     npm uninstall @uber/create-uber-web --global || true;
     npm install @uber/create-uber-web --global || true;
   `);
-  // eslint-disable-next-line no-console
-  console.log('Upgrade complete. Please rerun this command.');
-  process.exit();
-}
+    // eslint-disable-next-line no-console
+    console.log('Upgrade complete. Please rerun this command.');
+    process.exit();
+  }
 
-require('./index.js');
+  require('./index.js');
+})();
