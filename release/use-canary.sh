828311ac2881670ef2ac71f5ed71be0152b18196
UberOpenSourceBot <UberOpenSourceBot@users.noreply.github.com>
2019-09-06 19:52:33 +0000

    Add use-canary and scaffold test scripts for release testing (#491) https://github.com/uber/fusionjs/pull/491
    This PR automates the scaffolding and canary installation steps described in the setup / testing sections of the [Fusion Release Runbook](https://docs.google.com/document/d/1bn82miUvafzBKJRMyLPKFdutR4G-9_hJjGHPcdXVBgM/edit#heading=h.4x8n)
    
    Co-authored-by: Albert Wu <wua@uber.com>

---
+++ release/use-canary.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# 
+# Applies the specified fusion canary release to a package.json file.
+#
+# Inputs:
+# - the desired canary release version
+# - the path to a package.json file
+#
+# Example:
+# ./use-canary 0.0.0-canary.109b061.0 ~/Documents/code/test-proj/package.json
+#
+
+CANARY_RELEASE=$1
+PACKAGE_JSON=$2
+
+# since our rush.json manifest is not standard json (has comments), we use the any-json library to parse it
+npm install -g any-json
+
+function hasCanaryRelease() {
+  local PKG=$1
+  local CANARY_RELEASE=$2
+  npm view "$PKG" versions 2> /dev/null | \
+  tr "'" '"' | \
+  jq -e --arg CANARY_RELEASE "$CANARY_RELEASE" 'contains([$CANARY_RELEASE])' > /dev/null
+}
+
+TMP=$(mktemp)
+for PKG in $(any-json rush.json | jq -r '.projects[].packageName')
+do
+  if hasCanaryRelease $PKG $CANARY_RELEASE; then
+    echo "$PKG has $CANARY_RELEASE -- updating package.json"
+    cat $PACKAGE_JSON | jq --arg PKG "$PKG" --arg CANARY_RELEASE "$CANARY_RELEASE" '.devDependencies=.devDependencies + (.devDependencies | to_entries | map(select(.key | match($PKG)) |= {"key": .key, "value": $CANARY_RELEASE}) | from_entries)' > $TMP && mv $TMP $PACKAGE_JSON
+    cat $PACKAGE_JSON | jq --arg PKG "$PKG" --arg CANARY_RELEASE "$CANARY_RELEASE" '.dependencies=.dependencies + (.dependencies | to_entries | map(select(.key | match($PKG)) |= {"key": .key, "value": $CANARY_RELEASE}) | from_entries)' > $TMP && mv $TMP $PACKAGE_JSON
+  fi
+done
+
