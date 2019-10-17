53c0056b228958f92871a650c47b0da291b73dfc
Albert Wu <wua@uber.com>
2019-10-17 17:10:49 +0000

    Add integration tests for scaffolds (#605) https://github.com/uber/fusionjs/pull/605
    Add integration tests to `yarn test` for `website`, `library`, `fusion-plugin`. For now, omitting `website-graphql` since it currently does not pass tests.

--- private/create-uber-web/verify.sh
+++ private/create-uber-web/verify.sh
@@ -1,15 +1,45 @@
+#!/usr/bin/env bash
+
 echo "Verifying...";
-for d in ./templates/*/ ; do (
-  cd "$d";
-  yarn >/dev/null 2>/dev/null;
-  yarn test >/dev/null 2>/dev/null &&
-  yarn lint >/dev/null 2>/dev/null &&
-  yarn flow >/dev/null 2>/dev/null &&
-  echo -e "$d \033[0;32mOK\033[0m" ||
-  echo -e "$d \033[0;31mFAILED\033[0m" && exit 1;
-  rm -rf node_modules;
-  rm -rf dist;
-  rm -f yarn-error.log;
-  rm -rf .fusion;
-); done
-exit 0;
+
+# install
+jazelle ci && jazelle build
+
+TYPES=(
+  website
+  fusion-plugin
+  library
+)
+
+for TYPE in ${TYPES[*]};
+do 
+  echo "Testing $TYPE"
+  rm -rf test-project
+
+  node dist/index.js scaffold \
+  --type "$TYPE" \
+  --name "test-project" \
+  --description "my project" \
+  --team web \
+  --audience internal \
+  --skip-install
+
+  (
+    cd test-project
+    yarn --ignore-engines
+    yarn --ignore-engines test && \
+    yarn --ignore-engines lint && \
+    yarn --ignore-engines flow
+  )
+  code=$?
+  rm -rf test-project
+  if [ $code -eq 0 ]
+  then
+    echo -e "$TYPE" "\033[0;32mOK\033[0m"
+  else
+    echo -e "$TYPE" "\033[0;31mFAILED\033[0m"
+    exit 1
+  fi
+done
+
+exit 0
