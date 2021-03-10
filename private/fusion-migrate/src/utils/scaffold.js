f074fc429151e09d12338dd394d600d0d9b6b51e
Sujaan Singh <sujaan@uber.com>
2021-03-10 18:59:31 +0000

    Corrects fusion migration scaffold template path (#1218) https://github.com/uber/fusionjs/pull/1218

--- private/fusion-migrate/src/utils/scaffold.js
+++ private/fusion-migrate/src/utils/scaffold.js
@@ -5,5 +5,5 @@ const execa = require('execa');
 module.exports = async function scaffold(options = {}) {
   const dirname = tmp.dirSync().name;
   await execa.shell(`git clone git@github.com:uber/fusionjs.git ${dirname}`);
-  return path.join(dirname, 'private/create-uber-web/templates/website');
+  return path.join(dirname, 'private/template-website');
 };
